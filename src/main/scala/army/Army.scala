94bba0300add930f134c73c2a2d2742c34052340
vichobicos <vicente.dgaona@gmail.com>
2026-03-19 22:04:34 -0300

    feat: added army

--- src/main/scala/army/Army.scala
+++ src/main/scala/army/Army.scala
@@ -2,12 +2,16 @@ package army
 
 import soldier.Soldier
 
-class Army {
+import scala.collection.mutable.ArrayBuffer
+
+class Army(val name: String) {
+  var soldiers: ArrayBuffer[Soldier] = new ArrayBuffer[Soldier]
   def join(soldier: Soldier): Unit = {
-    
+    soldiers.addOne(soldier)
   }
   
   def leave(soldier: Soldier): Unit = {
-    
+    val index: Int = soldiers.indexOf(soldier)
+    soldiers.remove(index)
   }
 }
